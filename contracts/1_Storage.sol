b7d1818bc9ee48924bc490a270f6533b1867b2dd
Ruzell11 <johnruzell123@gmail.com>
2023-07-04 07:19:18 +0800

    put different usage of data types

--- contracts/1_Storage.sol
+++ contracts/1_Storage.sol
@@ -4,5 +4,9 @@ pragma solidity  ^0.8.18;
 //types of data types
 //boolean , unit , int , address , bytes , string
 contract SimpleStorage {
-    bool hasFavoriteNumber;
+    bool hasFavoriteNumber = true;
+    uint favoriteNumber = 123;
+    string myName = "Ruzell";
+    bytes32 favoriteBytes = "cat";
+    address myAddress = 0xDe15B3b15177Aee9314941F477C14Cf83D99a5Ef;
 }
