bf5f26eca47702b2672ed0e4e90e4db1d09bcf53
Ruzell11 <johnruzell123@gmail.com>
2023-07-03 16:03:12 +0800

    initial commit

---
+++ contracts/1_Storage.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity  ^0.8.18;
+
+//types of data types
+//boolean , unit , int , address , bytes , string
+contract SimpleStorage {
+    bool hasFavoriteNumber;
+}
