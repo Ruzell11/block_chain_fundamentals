43007c336e78ab1369e827d096aabeceedb767ca
Ruzell11 <johnruzell123@gmail.com>
2023-07-06 16:41:08 +0800

    Implemented inheritance override keyword to override exisint function from the parent

---
+++ contracts/ExtraStorage.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity  ^0.8.18;
+import "contracts/SimpleStorage.sol";
+
+//inheritance
+contract ExtraStorage is SimpleStorage {
+    //override
+
+    //public override
+}
